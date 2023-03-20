ea4de80c89bca0dd87ebe63d71c226f58b9d8479
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-03-20 12:28:17 -0400

    Se desarrollo el segundo ciclo de TDD para el numero 2

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,4 +4,7 @@ describe("FizzBuzz",()=>{
     it("genera para el 1",()=>{
         expect(fizzbuzz(1)).toEqual("1");
     })
+    it("genera para el 2",()=>{
+        expect(fizzbuzz(2)).toEqual("2");
+    })
 })
