587ee7ff3638a3cdbe4379135b576cafcf79232f
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-03-20 12:29:40 -0400

    Se desarrollo el tercer ciclo de TDD para el numero 3

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,4 +7,7 @@ describe("FizzBuzz",()=>{
     it("genera para el 2",()=>{
         expect(fizzbuzz(2)).toEqual("2");
     })
+    it("genera Fizz para 3",()=>{
+        expect(fizzbuzz(3)).toEqual("Fizz");
+    })
 })
